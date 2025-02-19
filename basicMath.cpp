dc941cd0a6f85697acc13ee392bcb2b3a038ab8e
Serge Hamouche <Serge.el-hamouche@stu.ocsb.ca>
2025-02-19 18:27:24 +0000

    basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2020 Serge All right reserved
+//
+// Created By : Your Name
+// Date : Month Day, Year
+// Write what this program does.
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "9+2=" << (9 + 2) << std::endl;
+    std::cout << "7-2=" << (7 - 2) << std::endl;
+    std::cout << "4/2=" << (4 / 2) << std::endl;
+    std::cout << "4+4/2=" << (4 + 4 / 2) << std::endl;
+    std::cout << "3+4*2=" << (3 + 4 * 2) << std::endl;
+    std::cout << "5+2^3=" << (5 + 2 ^ 3) << std::endl;
+    std::cout << "5+pow(2,3)=" << (5+pow(2,3)) << std::endl;
+}
